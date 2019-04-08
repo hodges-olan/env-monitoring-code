8064c7b32c743d643c7c986a7f6f25d9154118bc
Olan Hodges <olanhodges@gmail.com>
2019-04-08 14:38:45 -0500

    Create temphumd.py

---
+++ temphumd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import Adafruit_DHT as dht
+from datetime import datetime
+humidity, temperature = dht.read_retry(dht.AM2302, 4)
+temperature = temperature * 9/5.0 + 32
+localtime = datetime.now()
+if humidity is not None and temperature is not None:
+    output = '{{ "index" : "MasterBedroom-{0}", "datetime" : "{0}", "location" : "MasterBedroom", "Temp
+" : "{1:0.1f}", "Humidity" : "{2:0.1f}%" }}'.format(localtime, temperature, humidity)
+else:
+    output = 'Failed to get reading. Try again!'
+fh = open("/home/svcnodered/output.txt", "a")
+fh.write(output)
+fh.close()
