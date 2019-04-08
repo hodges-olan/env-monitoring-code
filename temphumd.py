7366a24ca360448ad0d0def0d811f80abeb12390
Olan Hodges <olanhodges@gmail.com>
2019-04-08 15:22:55 -0500

    Update temphumd.py

--- temphumd.py
+++ temphumd.py
@@ -5,8 +5,7 @@ humidity, temperature = dht.read_retry(dht.AM2302, 4)
 temperature = temperature * 9/5.0 + 32
 localtime = datetime.now()
 if humidity is not None and temperature is not None:
-    output = '{{ "index" : "MasterBedroom-{0}", "datetime" : "{0}", "location" : "MasterBedroom", "Temp
-" : "{1:0.1f}", "Humidity" : "{2:0.1f}%" }}'.format(localtime, temperature, humidity)
+    output = '{{ "index" : "MasterBedroom-{0}", "datetime" : "{0}", "location" : "MasterBedroom", "Temp" : "{1:0.1f}", "Humidity" : "{2:0.1f}%" }}'.format(localtime, temperature, humidity)
 else:
     output = 'Failed to get reading. Try again!'
 fh = open("/home/svcnodered/output.txt", "a")
